42c3047cc4ef8ada85cc5b09afe18f722e9b1377
pedrokappes <pedrokappes@gmail.com>
2022-10-24 17:16:00 -0300

    instalando o sdk

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,22 @@
 import 'package:flutter/material.dart';
 
+import 'package:parse_server_sdk_flutter/parse_server_sdk.dart';
+
 import 'Screens/LoginAndRegister/LoginForm.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
-void main() {
+void main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+  final keyApplicationId = 'ESkRBjvIlTDkiGnWAJK5CUP9BrxmMN0fsF8cwP3H';
+  final keyClientKey = 'UAhW55dzuEhJQqjB6oh0phhqgXIBqogFLg0iHsEr';
+  final keyParseServerUrl = 'https://parseapi.back4app.com';
+
+  await Parse().initialize(keyApplicationId, keyParseServerUrl, clientKey: keyClientKey, autoSendSessionId: true);
+
+  // var firstObject = ParseObject('FirstClass')
+  //   ..set('message', 'Hey ! First message from Flutter. Parse is now connected');
+  // await firstObject.save();
   runApp(const MyApp());
 }
 
